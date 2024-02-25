070977cbf2c5255808174c32831ac825ade4c84e
rachityadav730 <rachit.r.yadav@gmail.com>
2024-02-25 23:33:27 +0530

    add gitignore file

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const heading = React.createElement('H1',{id: "Heading", erc: "hsdfadsf"}, React.createElement('H1',{id: "Child", erc: "hsdfadsf"},[
-    React.createElement('H1',{}, "Hello 1"),
-    React.createElement('H2', {}, "Hello 2 1")
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
+const heading = React.createElement('h1',{id: "Heading", erc: "hsdfadsf"},
+ React.createElement('h1',{id: "Child", erc: "hsdfadsf"},[
+    React.createElement('h1',{}, "Hello 1"),
+    React.createElement('h2', {}, "Hello 2 1")
     ]))
 
     const rootElement = ReactDOM.createRoot(document.getElementById('root'))
